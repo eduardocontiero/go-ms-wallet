ec4342110b285e2656dec2314a6e9d811da1496b
Eduardo Contiero <eduardocontiero@hotmail.com>
2024-07-27 14:11:21 -0300

    create account entity

--- internal/entity/client_test.go
+++ internal/entity/client_test.go
@@ -35,3 +35,11 @@ func TestUpdateClientWhenArgsAreInvalid(t *testing.T) {
 	err := client.Update("", "j@j.com")
 	assert.Error(t, err, "name is required")
 }
+
+func TestAddAccountToClient(t *testing.T) {
+	client, _ := NewClient("John Doe", "j@j.com")
+	account := NewAccount(client)
+	err := client.AddAccount(account)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(client.Accounts))
+}
